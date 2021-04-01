036a12793f290d7fba2d6a76663d6c8e6e860efa
mokunshao <mokunshao@foxmail.com>
2021-04-02 00:50:04 +0800

    backup

--- content.js
+++ content.js
@@ -1,15 +1,13 @@
-(function () {
-  var re = /DATALIST|KEYGEN|INPUT|OPTION|SELECT|TEXTAREA/i;
-  window.onkeyup = function (event) {
-    if (event.key === 'Backspace') {
-      if (!re.test(event.target.tagName) && !event.target.isContentEditable) {
-        event.preventDefault();
-        if (event.shiftKey) {
-          window.history.forward();
-        } else {
-          window.history.back();
-        }
+var re = /DATALIST|KEYGEN|INPUT|OPTION|SELECT|TEXTAREA/i;
+window.onkeyup = function (event) {
+  if (event.key === 'Backspace') {
+    if (!re.test(event.target.tagName) && !event.target.isContentEditable) {
+      event.preventDefault();
+      if (event.shiftKey) {
+        window.history.forward();
+      } else {
+        window.history.back();
       }
     }
-  };
-})();
+  }
+};
