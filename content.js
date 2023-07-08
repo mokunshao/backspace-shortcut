90f34a58bf523f1d9e7dc44ddf7b9504f92de47c
mokunshao <mokunshao@foxmail.com>
2023-07-08 21:07:20 +0800

    disable for customElement

--- content.js
+++ content.js
@@ -1,13 +1,16 @@
 var re = /DATALIST|KEYGEN|INPUT|OPTION|SELECT|TEXTAREA/i;
 window.onkeyup = function (event) {
   if (event.key === 'Backspace') {
-    if (!re.test(event.target.tagName) && !event.target.isContentEditable) {
-      event.preventDefault();
-      if (event.shiftKey) {
-        window.history.forward();
-      } else {
-        window.history.back();
-      }
+    const target = event.target;
+    const tagName = target.tagName;
+    if (re.test(tagName) || target.isContentEditable || tagName.includes("-")) {
+      return;
+    }
+    event.preventDefault();
+    if (event.shiftKey) {
+      window.history.forward();
+    } else {
+      window.history.back();
     }
   }
 };
